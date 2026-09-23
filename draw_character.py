05dd4090f61861653b5f416101b48944b60049b0
이름 <>
2026-09-23 10:48:12 +0900

    출력 순서 확인

--- draw_character.py
+++ draw_character.py
@@ -1,2 +1,13 @@
-while True:
+def move_circle():
     pass
+
+def move_rectangle():
+    pass
+
+def move_triangle():
+    pass
+
+while True:
+    move_circle()
+    move_rectangle()
+    move_triangle()
